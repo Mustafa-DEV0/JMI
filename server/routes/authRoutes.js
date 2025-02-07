79132887907af6d4bd5d1bd787143f0a4d8e03f7
Mustafa-DEV0 <mm722310@gmail.com>
2025-02-07 16:43:51 +0530

    auth contoller setup

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,7 @@
-const express = require("express");
-const { registerUser, loginUser, getUserProfile } = require("../controllers/authController");
-const { protect } = require("../middleware/authMiddleware");
-
+import express from "express";
+import registerUser from "../controllers/registerController";
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/profile", protect, getUserProfile);
+router.post("/signup", registerUser);
 
-module.exports = router;
+export default router;
