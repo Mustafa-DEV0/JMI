2e8f1d946c85e0509b9586f6b367a92ed8715d87
Mustafa-DEV0 <mm722310@gmail.com>
2025-02-07 20:56:19 +0530

    fix:minor bug

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import axios from "axios";
 import styles from "./Signup.module.css";
 import { Link, useNavigate } from "react-router-dom";
