b1d7644cfdb53010e7fef0768145d72afdc8c4be
Naurezzz <mohammednaurez04@gmail.com>
2025-02-07 22:38:56 +0530

    Doctor details page done!

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "../pages/signup/Signup.jsx";
 import Login from "../pages/login/login.jsx";
 import Home from "../pages/home/Home.jsx";
+import Doctor from "../pages/doctor/Doctor.jsx";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/doctor" element={<Doctor />} />
     </Routes>
   );
 };
