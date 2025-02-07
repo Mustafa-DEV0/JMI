33e83ff18de92d3faebd00d5e1d59072168a3a60
samrod625 <samrod625@gmail.com>
2025-02-07 20:41:50 +0530

    Added Models

---
+++ server/models/Appointment.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    requestedAt: { type: Date, default: Date.now }, // When the appointment was requested
+    scheduledAt: { type: Date }, // When the appointment is actually scheduled
+    status: {
+      type: String,
+      enum: ["pending", "scheduled", "completed", "cancelled"],
+      default: "pending",
+    },
+    isPaid: { type: Boolean, default: false },
+    prescription: { type: mongoose.Schema.Types.ObjectId, ref: "Prescription" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Appointment", appointmentSchema);
