cc01f91671065fa69d3f489a1564d69586420dd3
Mustafa-DEV0 <mm722310@gmail.com>
2025-02-07 20:56:19 +0530

    fix:minor bug

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "../pages/login/Login.jsx";
 import Signup from "../pages/signup/Signup.jsx";
+import Login from "../pages/login/login.jsx";
 import Home from "../pages/home/Home.jsx";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/register" element={<Signup />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
