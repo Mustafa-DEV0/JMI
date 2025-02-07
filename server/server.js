e56dcd300c4ca97509426c9e56fd3f74482c0e46
Mustafa-DEV0 <mm722310@gmail.com>
2025-02-07 16:57:35 +0530

    update:backend server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const dotenv = require("dotenv");
-const app = require("./app");
+import dotenv from 'dotenv';
+import app from './app.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
