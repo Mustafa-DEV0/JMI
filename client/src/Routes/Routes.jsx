9cf5bcf886e356a27811ff8bce2189ee798c927d
Naurezzz <mohammednaurez04@gmail.com>
2025-02-08 10:04:54 +0530

    Medical store routes made!

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -6,6 +6,8 @@ import Doctor from "../pages/doctor/Doctor.jsx";
 import Patient from "../pages/patient/Patient.jsx";
 import Prescription from "../pages/prescription/Prescription.jsx";
 import DoctorList from "../pages/doctorlist/DoctorList.jsx";
+import MedicalStore from "../../../server/models/MedicalStore.js";
+
 const AppRoutes = () => {
   return (
     <Routes>
@@ -17,6 +19,7 @@ const AppRoutes = () => {
       <Route path="/prescription/:id" element={<Prescription />} />
       <Route path="/prescription/:id" element={<Prescription />} />
       <Route path="/doctorlist" element={<DoctorList />} />
+      <Route path="/medicalstore" element={<MedicalStore />} />
     </Routes>
   );
 };
