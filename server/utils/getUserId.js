e322f8800bb2244d503ab0559b26560a67ecb609
samrod625 <samrod625@gmail.com>
2025-02-08 22:14:19 +0530

    Appointment history updated

--- server/utils/getUserId.js
+++ server/utils/getUserId.js
@@ -3,9 +3,8 @@ import jwt from "jsonwebtoken";
 const getUserIdFromJwt = (token) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded Token:", decoded); // Debugging line
 
-    return decoded.id; // Ensure 'userId' exists in the payload
+    return decoded.id;
   } catch (error) {
     console.error("Error verifying JWT:", error);
     return null;
