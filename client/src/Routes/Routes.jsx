4f0dae933a1706c73a1aa0814e5076d9d123f2a2
Imaad Baig <nnm23cs253@nmamit.in>
2025-02-08 15:06:09 +0530

    medical info  page done

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -8,7 +8,9 @@ import Prescription from "../pages/prescription/Prescription.jsx";
 import DoctorList from "../pages/doctorlist/DoctorList.jsx";
 import MyAppointment from "../pages/myappointment/Myappointment.jsx"; // Ensure filename matches exactly
 import MedicalStore from "../pages/medicalstore/MedicalStore.jsx";
-import Appointment from "../pages/appointment/Appointment.jsx"; // Ensure filename matches exactly
+import Appointment from "../pages/Appointment/appointment.jsx";
+import MedicalOrder from "../pages/medicalOrder/Medicalorder.jsx";
+import Medicalowner from "../pages/medicalowner/Medicalowner.jsx";
 
 const AppRoutes = () => {
   return (
@@ -23,6 +25,9 @@ const AppRoutes = () => {
       <Route path="/myappointment" element={<MyAppointment />} />
       <Route path="/medicalstore" element={<MedicalStore />} />
       <Route path="/appointment" element={<Appointment />} />
+      <Route path="/medicalOrder" element={<MedicalOrder />} />
+      <Route path="/medicalowner" element={<Medicalowner />} />
+
       
     </Routes>
   );
