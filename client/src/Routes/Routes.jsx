ad7018ddfb4e6a70babe9e477f2d7ade61f3c777
Imaad Baig <nnm23cs253@nmamit.in>
2025-02-08 12:59:10 +0530

    my appointment page done

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "../pages/signup/Signup.jsx";
-import Login from "../pages/login/login.jsx";
+import Login from "../pages/login/Login.jsx";  // Fixed case issue
 import Home from "../pages/home/Home.jsx";
 import Doctor from "../pages/doctor/Doctor.jsx";
 import Patient from "../pages/patient/Patient.jsx";
 import Prescription from "../pages/prescription/Prescription.jsx";
 import DoctorList from "../pages/doctorlist/DoctorList.jsx";
-import MedicalStore from "../pages/medicalstore/MedicalStore.jsx";
-import MedicalPrescription from "../pages/medicalprescription/MedicalPrescription.jsx";
-import Appointment from "../pages/appointment/Appointment.jsx";
+import MyAppointment from "../pages/myappointment/Myappointment.jsx"; // Ensure filename matches exactly
 
 const AppRoutes = () => {
   return (
@@ -18,13 +16,9 @@ const AppRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/doctor/:id" element={<Doctor />} />
       <Route path="/patient/:id" element={<Patient />} />
-      <Route path="/prescription" element={<Prescription />} />
+      <Route path="/prescription/:id" element={<Prescription />} />
       <Route path="/doctorlist" element={<DoctorList />} />
-      <Route path="/medicalstore" element={<MedicalStore />} />
-      <Route path="/medicalprescription" element={<MedicalPrescription />} />
-      <Route path="/appointment" element={<Appointment />} />
-
-
+      <Route path="/myappointment" element={<MyAppointment />} />
     </Routes>
   );
 };
