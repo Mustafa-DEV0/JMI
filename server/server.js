392619326807c03d08caa21ddec92eb98e7410a8
samrod625 <samrod625@gmail.com>
2025-02-12 23:14:47 +0530

    Deployed Bcakend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,37 @@
-import dotenv from 'dotenv';
-import app from './app.js';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import patientRoutes from "./routes/patientRoutes.js";
+import doctorRoutes from "./routes/doctorRoutes.js";
+import appointmentRoutes from "./routes/appointmentRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 dotenv.config();
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/patient", patientRoutes);
+app.use("/doctor", doctorRoutes);
+app.use("/appointment", appointmentRoutes);
+app.use("/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
